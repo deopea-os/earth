d414a9ebb0692ae8a298030b6b47c29691971431
David <david@deopea.dev>
2023-01-20 22:52:10 +0000

    refactor: many changes for semantic release configs

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,6 +3,16 @@ if (!process.env.CI) {
   husky.install();
 
   const { exec } = await import("child_process");
-  const proc = exec("npm run -w packages/commitlint-config build");
+  const proc = exec(
+    [
+      "npm run",
+      "-w packages/commitlint-config",
+      "-w packages/conventional-changelog",
+      "-w packages/eslint-config",
+      "-w packages/prettier-config",
+      "-w packages/semantic-release-config",
+      "build",
+    ].join(" "),
+  );
   proc.stdout?.pipe(process.stdout);
 }
