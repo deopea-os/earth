abd23d7a568781993d262a2b7299aaa3edaa189e
David <david@deopea.dev>
2023-02-27 23:10:04 +0000

    build(apps/earth): fixes docker image asset

--- apps/earth/stack/client.ts
+++ apps/earth/stack/client.ts
@@ -1,11 +1,8 @@
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { join } from "path";
-import { fileURLToPath } from "url";
-
-export const ClientCode = lambda.DockerImageCode.fromImageAsset(
-  join(fileURLToPath(import.meta.url), "..", ".."),
-);
+import { cwd } from "process";
 
 export class ClientLambda extends Construct {
   handler: lambda.DockerImageFunction;
@@ -16,8 +13,14 @@ export class ClientLambda extends Construct {
     props: { env: "production" | "staging" },
   ) {
     super(scope, id);
+
+    const ClientCode = new DockerImageAsset(this, "ClientImage", {
+      directory: cwd(),
+      file: join("apps", "earth", "Dockerfile"),
+    });
+
     this.handler = new lambda.DockerImageFunction(this, "ClientHandler", {
-      code: ClientCode,
+      code: lambda.DockerImageCode.fromEcr(ClientCode.repository),
       environment: props,
     });
   }
